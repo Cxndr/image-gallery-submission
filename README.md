0cb5a439ae1707b29cf7f57e2996529633af14dc
Matt Vandersluys <matt.matiksaudio@gmail.com>
2024-08-26 00:35:18 +0100

    finished project - just need to test screen reader on windows/mac.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Deez Sloths
+
+## Requirements
+
+### 🎯 Implement responsive design using media queries to ensure the website works well on both small mobile screens and larger desktop screens (e.g., above 800px).
+
+I've designed initially for mobile first, and then used media queries for `min-width: 1025px` for desktop. The decision to use 1025px instead od 800px excludes very old 3:4 desktop monitors with resolutions like 1024 x 768 and will force these to display a mobile version of the site. However 1025 also excludes new ipad models in portrait mode. This is a worthwhile tradeoff in my opinion as newer tablets will be most likely the more used user device of the two, and the tradeoff of a mobile display on old tech isn't a severe user issue in this case.
+
+I have also added media quieries for ultrawide monitors with `min-width: 2800px`.
+
+
+### 🎯 Ensure all images have appropriate alt text attributes for accessibility.
+
+All of the image gallery images have appropriate alt text taken from the photographers and also use the photographer's name in the alt text.
+
+### 🎯 Correctly use event handlers to switch images based on user interactions.
+
+You can switch through images by using the arrow keys, by clicking on each thumbnail image, or by focusing each thumbnail image and pressing enter.
+
+## Stretch Requirments
+
+### 🏹 Use 'srcset' to specify which image will be used based on the size of the screen for optimal viewing experience.
+When an image is loaded as the main image the correct srcset is applied and the image loaded will change on each device. To provide a better user experience on large resolution devices without high performance hardware or internet speed I have implemented a "loading" div in javascript that hides all image content until it is fully loaded. Without this the images at first load distorted and warped and then correct once the page has fully loaded which would look unproffessional.
+
+### 🏹 Add ARIA elements to improve accessibility, such as aria-label, aria-live, and other relevant attributes.
+I have included `aria-hidden` on the loading element as it is not nessecary to show for screen readers. I have also used `aria-live` with assertive setting on the main large image so the alt text is read out as the image is loaded. Usually I would use the polite setting but for this particular use case I think assertive is more appropriate as once a user has selected an image, the image should then imediately move to being the main focus.
+
+To facilitate a better user experience for screen reader users I have added `aria-label` through javascript to the thumbnails for each image. This overrides the default alt text being read and instead reads out the image number and photographer and prompts the user to select the image to load it, where the alt text for the actual image will then be read out using `aria-live`. Whilst there is an argument for keeping the alt text on the thumbnails, and i would do so for most other websites - I feel this approach is more appropriate for an image gallery website.
+
+### 🏹 Implement key bindings for buttons to enhance navigation, such as using arrow keys to switch between images.
+Arrow keys can be used to switch between images as mentioned above. This is implemented with javascript event listeners and functions that move a pointer along, unless we are at the start of end of the image list.
+
+### 🏹 Develop media queries for multiple screen sizes.
+As mentioned above the website is designed for mobile-first and then media queries are implemented for both standard desktop resolutions as well as ultrawide screens.
+
+## Reflections
+
+Given more time on the project I would have liked to implement on-screen buttons to navigate left and right through the images. This would be a realtively simple implementation utilizing the javascript functions already built and using button click events to call them in the same way arrow key presses do currently.
+
+I would also have liked to fix the image thumbnails horizontal scrollbar visually for chrome and webkit based browsers. Currently in firefox the scrollbar works as I would like: the images take up the full height with no scrollbar showing, and then when the user hovers over the thumbnails element the scrollbar appears on top of the thumbnail images - leaving the images in place. On chrome and webkit browsers however, the scrollbar will appear on hover and push the images up to make space for the scrollbar inside the thumbnails element. This would have previously been an easy fix using the `overflow: overlay` css property, but unfortunately this was discontinued a year ago and is no longer supported by chrome or edge browsers. The recommendation is to use `scrollbar-gutter` instead but in this specific case it is not appropriate as it can only reserve space for the scrollbar (not allow it to position over the element). If given more time and a better understanding of javascript libraries I would have liked to implement a javascript scrollbar replacement library such as SimpleBar, which replaces the default browser scrollbar for specific elements in a performant way, and allows the functionality I want across all browsers.
+
+I would also like to have implemented more aria-labels functionality, but struggled to think of additional appropriate use cases for the labels within the website design and specification.
--- app.js
+++ app.js
@@ -1,93 +1,130 @@
 class imageObject {
-    constructor(number, alt) {
-        this.number = number;
-        this.numberPadded = this.number.toString().padStart(2,'0');
-        this.alt = alt;
-        this.url = './img/' + this.numberPadded + '.jpg';
-        this.srcset = 
-            './img/640/'  + this.numberPadded + '.jpg' + ' 640w, '  + 
-            './img/1280/' + this.numberPadded + '.jpg' + ' 1280w, ' + 
-            './img/1920/' + this.numberPadded + '.jpg' + ' 1920w'
-        ;
-    }
+  constructor(number, creator, alt) {
+    this.number = number;
+    this.numberPadded = this.number.toString().padStart(2, "0");
+    this.creator = creator;
+    this.alt = alt;
+    this.url = "./img/" + this.numberPadded + ".jpg";
+    this.srcset =
+      "./img/640/" +
+      this.numberPadded +
+      ".jpg" +
+      " 640w, " +
+      "./img/1280/" +
+      this.numberPadded +
+      ".jpg" +
+      " 1280w, " +
+      "./img/1920/" +
+      this.numberPadded +
+      ".jpg" +
+      " 1920w, " +
+      "./img/" +
+      this.numberPadded +
+      ".jpg" +
+      " 2000w";
+  }
 }
 
 let images = [
-    new imageObject(
-        1, "alt-oqwiheoiqehwo"
-    )
-    ,
-    new imageObject(
-        2, "alt-fwoijfewoij"
-    )
-    ,
-    new imageObject(
-        3, "alt-qowiejrqwoij"
-    )
-    ,
-    new imageObject(
-        4, "alt-qowiejrqwoij"
-    )
-    ,
-    new imageObject(
-        5, "alt-qowiejrqwoij"
-    )
-    ,
-    new imageObject(
-        6, "alt-qowiejrqwoij"
-    )
+  new imageObject( 1, "Braulio Espinoza Sánchez", "A sloth is looking at the camera."),
+  new imageObject( 2, "Braulio Espinoza Sánchez", "Portrait of a sloth on a tree."),
+  new imageObject( 3, "David Gomez", "Brown-Throated sloth hanging upside down, looking at the camera."),
+  new imageObject( 4, "Heather Hathaway", "Sloth in Zoo"),
+  new imageObject( 5, "Ivy Marie", "Sloth and Branch"),
+  new imageObject( 6, "Denys Gromov", "Brown Sloth Hanging on the Metal Bar"),
+  new imageObject( 7, "Denys Gromov", "Brown Sloth in Close Up Photography"),
+  new imageObject( 8, "Jean Paul Montanaro", "A sloth hanging from a tree branch with its mouth open"),
+  new imageObject( 9, "Michael Linares", "A Brown Furry Animal Hanging on Tree Branch"),
+  new imageObject(10, "Miguel Cuenca", "Mother and Baby Sloth in Close-up Photography"),
+  new imageObject(11, "Osvaldo Madariaga", "Close-up of a Sloth Sitting on a Tree Trunk "),
+  new imageObject(12, "Palu Malerba", "Brown Sloth on the Tree"),
+  new imageObject(13, "Palu Malerba", "A Sloth on a Tree"),
+  new imageObject(14, "Roxanne Minnish", "Portrait of Sloth Hanging Upside Down on Branch"),
+  new imageObject(15, "Shuvalova Natalia", "Sloth Hanging on Tree"),
+  new imageObject(16, "Steven Paton", "Close up of Sloth"),
+  new imageObject(17, "Jay Turner", "Sloth Eating in Wild Nature"),
+  new imageObject(18, "Tomáš Malík", "Sloth on Tree"),
+  new imageObject(19, "Tomáš Malík", "Sloth Hanging on a Tree"),
+  new imageObject(20, "Tomáš Malík", "Sloth Lying on a Pillow")
 ];
 
-const thumbnailContainer = document.getElementById('thumbnail-container');
+const thumbnailContainer = document.getElementById("thumbnail-container");
 const imageContainer = document.getElementById('image-container');
+const loadingDiv = document.getElementById('loading');
 let currentImage = 0;
 
-images.forEach( function(img, index) {
-    const imageElement = document.createElement('img');
-    console.log(imageElement);
-    imageElement.src = img.url;
-    imageElement.srcset = img.srcset;
-    imageElement.alt = img.alt;
-    imageElement.tabIndex = 0;
-    
-    imageElement.addEventListener('click', function() {
-        changeImage(index);
-    });
-    imageElement.addEventListener('keydown', function(event) {
-        if (event.key == "Enter") {
-            changeImage(index);
-        }
-    })
+images.forEach(function (img, index) {
+  const imageElement = document.createElement("img");
+  console.log(imageElement);
+  imageElement.src = img.url;
+  imageElement.srcset = img.srcset;
+  imageElement.alt = img.alt;
+  imageElement.ariaLabel = `Image ${img.number} by ${img.author}: press enter to load`;
+  imageElement.tabIndex = 0;
 
-    thumbnailContainer.appendChild(imageElement);
+  imageElement.addEventListener("click", function () {
+    changeImage(index);
+  });
+  imageElement.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+      changeImage(index);
+    }
+  });
+
+  thumbnailContainer.appendChild(imageElement);
 });
 
 function changeImage(imageIndex) {
-    
-    imageContainer.style.backgroundImage = 'url(' + images[imageIndex].url + ')';
-    imageContainer.style.alt = images[imageIndex].alt;
-    if (currentImage != imageIndex) {currentImage = imageIndex};
+  imageContainer.src = images[imageIndex].url;
+  imageContainer.srcset = images[imageIndex].srcset;
+  imageContainer.alt = images[imageIndex].alt;
+  if (currentImage != imageIndex) {
+    currentImage = imageIndex;
+  }
 }
 function changeImageForward() {
-    if (currentImage >= images.length-1) { return; }
-    currentImage += 1;
-    changeImage(currentImage);
+  if (currentImage >= images.length - 1) {
+    return;
+  }
+  currentImage += 1;
+  changeImage(currentImage);
 }
 function changeImageBackward() {
-    if (currentImage <= 0) { return; }
-    currentImage -= 1;
-    changeImage(currentImage);
+  if (currentImage <= 0) {
+    return;
+  }
+  currentImage -= 1;
+  changeImage(currentImage);
 }
 
-document.addEventListener('keyup', function(event) {
-    console.log("before: " + currentImage);
-    switch (event.key) {
-        case 'ArrowLeft':
-            changeImageBackward();
-            break;
-        case "ArrowRight":
-            changeImageForward();
-            break;
-    }
-    console.log("after: " + currentImage);
+document.addEventListener("keyup", function (event) {
+  console.log("before: " + currentImage);
+  switch (event.key) {
+    case "ArrowLeft":
+      changeImageBackward();
+      break;
+    case "ArrowRight":
+      changeImageForward();
+      break;
+  }
+  console.log("after: " + currentImage);
+});
+
+// don't load image elements until page finished loading (to prevent displaying stretched images)
+document.addEventListener('DOMContentLoaded', function () {
+  imageContainer.style.display = "block";
+  thumbnailContainer.style.display = "flex";
+  loadingDiv.style.display = "none";
+})
+
+// horizontal scroll for thumbnails
+thumbnailContainer.addEventListener('wheel', function(event) {
+  if (event.deltaY > 0) {
+    thumbnailContainer.scrollLeft += 100;
+    event.preventDefault();
+  }
+  else {
+    thumbnailContainer.scrollLeft -= 100;
+    event.preventDefault();
+  }
 });
